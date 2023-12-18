b95edacd0017220dbec0f6ae18b2176989ffd210
Santiago Firpo <s.firpo@edu.pucrs.br>
2023-12-18 01:19:10 -0300

    Custom fonts

--- src/components/expositionText.tsx
+++ src/components/expositionText.tsx
@@ -1,7 +1,17 @@
+import { Inter, Hepta_Slab } from "@next/font/google"
+
+const inter = Inter
+(
+    {
+        subsets: ["latin"],
+        variable: "--font-inter"
+    }
+);
+
 export const ExpositionText = () =>
     <div className="w-screen flex flex-row justify-center gap-[215px]">
         <div
-            className="w-[434px] h-[535px] text-black text-[17px] not-italic font-semibold leading-[27px] pt-[118px]">Tenho
+            className={`w-[434px] h-[535px] text-black text-[17px] not-italic font-semibold leading-[27px] pt-[118px] ${inter.variable} font-inter`}>Tenho
             mais de 30 de experiência em comunicação, publicidade e branding, e já fui premiado em múltiplos
             festivais.<br/>Como CMO, sou o responsável pelo marketing e estratégias de prospecção de uma empresa
             global de soluções de TI e dados, atendendo a clientes nos EUA, Europa e LATAM.<br/>Como Diretor de
@@ -12,8 +22,8 @@ export const ExpositionText = () =>
             audiovisual são especialmente bem-vindos).
         </div>
         <div
-            className="w-[435px] text-black text-[17px] not-italic font-normal leading-[27px] tracking-[-0.17px] pt-[172px]"><span
-            className="text-black text-base font-semibold font-['Inter'] leading-relaxed pt-3">Alguns clientes atendidos:<br/></span><span
-            className="text-black text-base font-normal font-['Inter'] leading-relaxed">Grupo Telefónica, Petrobras, Vivo, Grupo Braskem, Edenred, Aegea, Banrisul, Grupo Gerdau, Midea-Carrier, Springer, GRU Airport, Husqvarna, Massey Ferguson, John Deere, Grupo RBS, Zero Hora, Ramarim, Klabin, Sicredi, Abicalçados, Sistema FIERGS, Paquetá, Gaston, Nutrella, Laghetto Hotéis, Piccadilly, Yara, Sescoop, Voopter, Op'n'Go, Vinhos Miolo, Oxiteno, Senac, Fecomércio, SC Internacional, UCS, Correio do Povo, Record, Hospital Moinhos de Vento, Governo do Rio Grande do Sul, Governo de Santa Catarina, Daer, Sulgás, Detran, CEEE, Corsan, Grupo Zaffari, Supermercados Angeloni, Tintas Renner, Sayerlack, Selenium, Cerâmicas Oxford, Student Travel Bureau, Lojas Colombo Stores, Rede de Concessionárias Fiat e as seguintes prefeituras: Porto Alegre, Passo Fundo, Bento Gonçalves, Gravataí, Santa Maria e Rio Grande. </span>
+            className={`w-[435px] text-black text-[17px] not-italic font-normal leading-[27px] tracking-[-0.17px] pt-[172px] ${inter.variable} font-inter`}><span
+            className="text-black text-base font-semibold leading-relaxed pt-3">Alguns clientes atendidos:<br/></span><span
+            className="text-black text-base font-normal leading-relaxed">Grupo Telefónica, Petrobras, Vivo, Grupo Braskem, Edenred, Aegea, Banrisul, Grupo Gerdau, Midea-Carrier, Springer, GRU Airport, Husqvarna, Massey Ferguson, John Deere, Grupo RBS, Zero Hora, Ramarim, Klabin, Sicredi, Abicalçados, Sistema FIERGS, Paquetá, Gaston, Nutrella, Laghetto Hotéis, Piccadilly, Yara, Sescoop, Voopter, Op'n'Go, Vinhos Miolo, Oxiteno, Senac, Fecomércio, SC Internacional, UCS, Correio do Povo, Record, Hospital Moinhos de Vento, Governo do Rio Grande do Sul, Governo de Santa Catarina, Daer, Sulgás, Detran, CEEE, Corsan, Grupo Zaffari, Supermercados Angeloni, Tintas Renner, Sayerlack, Selenium, Cerâmicas Oxford, Student Travel Bureau, Lojas Colombo Stores, Rede de Concessionárias Fiat e as seguintes prefeituras: Porto Alegre, Passo Fundo, Bento Gonçalves, Gravataí, Santa Maria e Rio Grande. </span>
         </div>
     </div>;
