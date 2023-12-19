31d8b76e3f8499d56e99d96a8680dbfaa1c936ff
Santiago Firpo <s.firpo@edu.pucrs.br>
2023-12-19 20:27:56 -0300

    Wrapped up landing page

--- src/components/bottomBar.tsx
+++ src/components/bottomBar.tsx
@@ -2,14 +2,15 @@ import Image from "next/image"
 
 export const BottomBar = () =>
 (
-    <div className="flex flex-col space-y-[11px] md:flex-row md:justify-center space-x-[11px]">
-        <div
-        className={`w-[412px] text-black text-xl font-semibold font-inter leading-normal`}>
+    <div className="flex flex-col space-y-[11px] items-center md:flex-row md:space-x-[11px]">
+        <p className={`w-[412px] text-black flex items-center text-xl font-semibold font-inter leading-normal`}>
             Resolva seus problemas de comunicação:
-        </div>
-        <div className="flex flex-row justify-center md:center space-x-[26px]">
-            <Image alt="email" src="/icons/email-icon.svg" width={38.48} height={34.2}/>
-            <Image alt="LinkedIn" src="/icons/linkedin-icon.svg" width={34.21} height={34.21}/>
+        </p>
+        <div className="h-full">
+            <div className=" flex flex-row items-center space-x-[26px]">
+                <Image alt="email" src="/icons/email-icon.svg" width={38.48} height={34.2}/>
+                <Image alt="LinkedIn" src="/icons/linkedin-icon.svg" width={34.21} height={34.21}/>
+            </div>
         </div>
     </div>
 );
