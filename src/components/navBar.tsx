b9dc73c1dfe52bd14afd47043b5f7f48b740f574
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2023-12-24 20:10:29 -0300

    Responsive changes

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
-import { emailLink, linkedInLink } from "../config";
+import { emailLink, linkedInLink } from "~/config";
 
 export const NavBar = () =>
 {
     return <div className="flex flex-row space-x-[26px]">
-        <Link href={emailLink}>
-            <Image alt="email" src="/icons/hero-icons/email-icon.svg" width={38.48} height={34.2}/>
-        </Link>
+        <div className="lg:w-[34px] lg:h-[30px] xl:w-[38px] xl:h-[34px]">
+            <Link href={emailLink}>
+                <Image alt="email" src="/icons/hero-icons/email-icon.svg" layout="fill"/>
+            </Link>
+        </div>
+        <div className="lg:w-[34px] lg:h-[30px] xl:w-[38px] xl:h-[34px]">
         <Link href={linkedInLink}>
             <Image alt="LinkedIn" src="/icons/hero-icons/linkedin-icon.svg" width={34.21} height={34.21}/>
         </Link>
+        </div>
     </div>;
 };
