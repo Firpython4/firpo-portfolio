2007980c9ff4bdec9f05f75f6d565c7881aebe4a
Santiago Firpo <s.firpo@edu.pucrs.br>
2023-12-24 02:46:24 -0300

    Made icons clickable

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,8 @@ export const publicFolderValue = "public";
 export const worksPath = `/${publicFolderValue}/works` as const;
 
 export type publicFolder = typeof publicFolderValue;
+
+export const linkedInLink = "https://www.linkedin.com/in/marcelofirpo/";
+
+const email = "marcelofirpo@gmail.com";
+export const emailLink = `mailto:${email}`
