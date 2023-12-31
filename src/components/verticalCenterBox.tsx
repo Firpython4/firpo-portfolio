622a15754608d2ec884fdd9a3a1539a93b80f933
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2023-12-31 19:25:40 -0300

    Classname cleanup and replaced space with gap

--- src/components/verticalCenterBox.tsx
+++ src/components/verticalCenterBox.tsx
@@ -1,5 +1,5 @@
 import { type PropsWithChildren } from "react";
 
-export const VerticalCenterBox = (props: PropsWithChildren<{className?: string}>) => <div className={`w-full flex flex-col items-center ${props.className}`}>
+export const VerticalCenterBox = (props: PropsWithChildren<{className?: string}>) => <div className={`${props.className} w-full flex flex-col items-center ${props.className}`}>
     {props.children}
 </div>;
