a113aa2ce98062d141e7fe0a045bc8e92ef11c61
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2023-12-30 23:01:15 -0300

    Initial video support

---
+++ src/localization/localization.tsx
@@ -0,0 +1,24 @@
+const pt_br = {
+    name: <>Marcelo Firpo</>,
+    subtitle: <>CMO, Diretor de Criação, Consultor Criativo, Redator, Roteirista</>,
+    expositionFirst: <>Tenho
+        mais de 30 de experiência em comunicação, publicidade e branding, e já fui premiado em múltiplos
+        festivais.<br/>Como CMO, sou o responsável pelo marketing e estratégias de prospecção de uma empresa
+        global de soluções de TI e dados, atendendo a clientes nos EUA, Europa e LATAM.<br/>Como Diretor de
+        Criação, trabalhei com grandes marcas do mercado brasileiro em segmentos como financeiro, energia,
+        administração pública, saúde, educação, serviços e varejo.<br/>Trabalhando feliz a partir de Lisboa,
+        Portugal, ofereço serviços em projetos relacionados a criatividade (branding, campanhas, ações,
+        direção de criação, redação) e produção textual em geral (projetos na área de entretenimento
+        audiovisual são especialmente bem-vindos).</>,
+    someAttendedClients: <>Alguns clientes atendidos:</>,
+    clients: <>Grupo Telefónica, Petrobras, Vivo, Grupo Braskem, Edenred, Aegea, Banrisul, Grupo Gerdau, Midea-Carrier,
+        Springer, GRU Airport, Husqvarna, Massey Ferguson, John Deere, Grupo RBS, Zero Hora, Ramarim, Klabin, Sicredi,
+        Abicalçados, Sistema FIERGS, Paquetá, Gaston, Nutrella, Laghetto Hotéis, Piccadilly, Yara, Sescoop, Voopter,
+        Op'n'Go, Vinhos Miolo, Oxiteno, Senac, Fecomércio, SC Internacional, UCS, Correio do Povo, Record, Hospital Moinhos
+        de Vento, Governo do Rio Grande do Sul, Governo de Santa Catarina, Daer, Sulgás, Detran, CEEE, Corsan, Grupo
+        Zaffari, Supermercados Angeloni, Tintas Renner, Sayerlack, Selenium, Cerâmicas Oxford, Student Travel Bureau, Lojas
+        Colombo Stores, Rede de Concessionárias Fiat e as seguintes prefeituras: Porto Alegre, Passo Fundo, Bento Gonçalves,
+        Gravataí, Santa Maria e Rio Grande.</>
+}
+
+export const locale = pt_br;
