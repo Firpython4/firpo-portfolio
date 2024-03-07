40ec70995d5b498421a2c9c828c338afd9ee62dc
Santiago Firpo <santiagofirpo4@gmail.com>
2024-03-07 16:08:13 -0300

    Testing gap on portfolio grid

--- src/components/pieceCollection.tsx
+++ src/components/pieceCollection.tsx
@@ -5,7 +5,7 @@ import { PiecePreview } from "./piecePreview";
 export const PieceCollection = (props: {pieces: PieceType<string>[], locale: Locale}) =>
 (
     <>
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-0">
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-1">
             {
                 props.pieces.map((piece: PieceType<string>) => <PiecePreview piece={piece} key={piece.url} locale={props.locale}/>)
             }
