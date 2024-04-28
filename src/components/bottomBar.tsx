13996967ae7647890bcb250e10d0902133044145
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:47:59 -0300

    localization v1

--- src/components/bottomBar.tsx
+++ src/components/bottomBar.tsx
@@ -8,7 +8,7 @@ export const BottomBar = (props: {className?: string, locale: Locale}) =>
     <div
         className={`${props.className} flex flex-col gap-y-[11px] items-center justify-center md:flex-row md:gap-x-[11px]`}>
         <p className={`xl:w-[412px] text-black text-center flex items-center text-lg ${responsiveText} font-semibold font-inter leading-normal`}>
-            {useLocaleTexts().solveYourCommunicationProblems}
+            {useLocaleTexts(props.locale).solveYourCommunicationProblems}
         </p>
         <NavBar iconPaths={{email: "/icons/email-icon.svg", linkedin: "/icons/linkedin-icon.svg"}}/>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
+import type { GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { type PieceType } from "~/types/pieceType";
+import {getIndexProps} from "~/index";
+import { type Locale } from "~/localization/localization";
+import Home from "./[locale]";
 
-const RedirectToLocale = () =>
+const FallbackHome = (props: HomeProps) =>
 {
     const router = useRouter();
     useEffect(() => void router.push(`/pt-BR${router.route}`))
-    return <>Redirecting...</>
-}
+    return (
+        <Home locale={props.locale} pieces={props.pieces}/>
+    );
+};
 
-export default RedirectToLocale;
+export const getStaticProps = (async () =>
+{
+    return await getIndexProps("pt-BR");
+    
+}) satisfies GetStaticProps<HomeProps>
+
+type HomeProps = {
+    pieces: PieceType[],
+    locale: Locale
+}
+export default FallbackHome
