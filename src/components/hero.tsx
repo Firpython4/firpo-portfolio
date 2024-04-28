5a606d3d4a7895c6583781c845934bc550302225
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:49:51 -0300

    Wrapped up landing page

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,16 +8,16 @@ export const Hero = () =>
     <div className=" w-full overflow-hidden">
         <HorizontalCenter>
             <div className="relative w-[1920px] h-[933px]">
-                <div className="absolute w-[1920px] h-[933px] bg-gradient-to-r from-black/5 to-black/10"/>
+                <div className="absolute bg-gradient-to-r from-black/5 to-black/10"/>
                 <div className="relative top-[-120px]">
                     <Image width={1920} height={933} src={background.src} alt="Marcelo Firpo"/>
                 </div>
-                <div className={`absolute w-[752px] shrink-0 text-white text-[107px] not-italic font-semibold leading-[98px] tracking-[-3.745px] left-[54.63%] top-[27.22%]  font-hepta_slab`}>
+                <h2 className={`absolute w-[752px] shrink-0 text-white text-[107px] not-italic font-semibold leading-[98px] tracking-[-3.745px] left-[54.63%] top-[27.22%]  font-hepta_slab`}>
                     Marcelo Firpo
-                </div>
-                <div className={`absolute w-[500px] h-[345px] text-white text-[44px] not-italic font-bold leading-[46.5px] left-[54.73%] top-[54.76%] font-hepta_slab`}>
+                </h2>
+                <h3 className={`absolute w-[500px] h-[345px] text-white text-[44px] not-italic font-bold leading-[50px] left-[54.73%] top-[54.76%] font-hepta_slab`}>
                     CMO, Diretor de Criação, Consultor Criativo, Redator, Roteirista
-                </div>
+                </h3>
                 <NavBar/>
             </div>
         </HorizontalCenter>
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { useBreakpoint } from "../hooks/tailwind";
 
 export const NavBar = () =>
 {
