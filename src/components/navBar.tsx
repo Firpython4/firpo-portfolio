8c36d5feb161b839e80727affa33015bdaeab23b
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:47:54 -0300

    Classname cleanup and replaced space with gap

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { emailLink, linkedInLink } from "~/config";
 
-export const NavBar = () =>
+export const NavBar = (props: {className?: string}) =>
 {
     return (
-        <div className="flex flex-row space-x-[26px]">
+        <div className={`${props.className} flex flex-row gap-x-[26px]`}>
             <div className="relative lg:w-[34px] lg:h-[30px] xl:w-[38px] xl:h-[34px]">
                 <Link href={emailLink}>
                     <Image alt="email" src="/icons/hero-icons/email-icon.svg" fill={true}/>
