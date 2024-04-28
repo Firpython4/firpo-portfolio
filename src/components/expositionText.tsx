da9512131abb62608088acb094bf2d85be689ef7
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:47:52 -0300

    Changed from text-base to text-small

--- src/components/expositionText.tsx
+++ src/components/expositionText.tsx
@@ -2,7 +2,7 @@ export const ExpositionText = () =>
 (
     <div className="w-full flex flex-col md:flex-row justify-center md:gap-[215px]">
         <div
-            className={`w-[434px] h-[535px] text-black text-[17px] not-italic font-semibold leading-[27px] pt-[118px] font-inter`}>Tenho
+            className={`w-[434px] h-[535px] text-black text-small not-italic font-semibold leading-[27px] pt-[118px] font-inter`}>Tenho
             mais de 30 de experiência em comunicação, publicidade e branding, e já fui premiado em múltiplos
             festivais.<br/>Como CMO, sou o responsável pelo marketing e estratégias de prospecção de uma empresa
             global de soluções de TI e dados, atendendo a clientes nos EUA, Europa e LATAM.<br/>Como Diretor de
@@ -13,9 +13,9 @@ export const ExpositionText = () =>
             audiovisual são especialmente bem-vindos).
         </div>
         <div
-            className={`w-[435px] text-black text-[17px] not-italic font-normal leading-[27px] tracking-[-0.17px] pt-[172px] font-inter`}><span
-            className="text-black text-base font-semibold leading-relaxed pt-3">Alguns clientes atendidos:<br/></span><span
-            className="text-black text-base font-normal leading-relaxed">Grupo Telefónica, Petrobras, Vivo, Grupo Braskem, Edenred, Aegea, Banrisul, Grupo Gerdau, Midea-Carrier, Springer, GRU Airport, Husqvarna, Massey Ferguson, John Deere, Grupo RBS, Zero Hora, Ramarim, Klabin, Sicredi, Abicalçados, Sistema FIERGS, Paquetá, Gaston, Nutrella, Laghetto Hotéis, Piccadilly, Yara, Sescoop, Voopter, Op'n'Go, Vinhos Miolo, Oxiteno, Senac, Fecomércio, SC Internacional, UCS, Correio do Povo, Record, Hospital Moinhos de Vento, Governo do Rio Grande do Sul, Governo de Santa Catarina, Daer, Sulgás, Detran, CEEE, Corsan, Grupo Zaffari, Supermercados Angeloni, Tintas Renner, Sayerlack, Selenium, Cerâmicas Oxford, Student Travel Bureau, Lojas Colombo Stores, Rede de Concessionárias Fiat e as seguintes prefeituras: Porto Alegre, Passo Fundo, Bento Gonçalves, Gravataí, Santa Maria e Rio Grande. </span>
+            className={`w-[435px] text-black text-small not-italic font-normal leading-[27px] tracking-[-0.17px] pt-[172px] font-inter`}><span
+            className="text-black text-small font-semibold leading-relaxed pt-3">Alguns clientes atendidos:<br/></span><span
+            className="text-black text-small font-normal leading-relaxed">Grupo Telefónica, Petrobras, Vivo, Grupo Braskem, Edenred, Aegea, Banrisul, Grupo Gerdau, Midea-Carrier, Springer, GRU Airport, Husqvarna, Massey Ferguson, John Deere, Grupo RBS, Zero Hora, Ramarim, Klabin, Sicredi, Abicalçados, Sistema FIERGS, Paquetá, Gaston, Nutrella, Laghetto Hotéis, Piccadilly, Yara, Sescoop, Voopter, Op'n'Go, Vinhos Miolo, Oxiteno, Senac, Fecomércio, SC Internacional, UCS, Correio do Povo, Record, Hospital Moinhos de Vento, Governo do Rio Grande do Sul, Governo de Santa Catarina, Daer, Sulgás, Detran, CEEE, Corsan, Grupo Zaffari, Supermercados Angeloni, Tintas Renner, Sayerlack, Selenium, Cerâmicas Oxford, Student Travel Bureau, Lojas Colombo Stores, Rede de Concessionárias Fiat e as seguintes prefeituras: Porto Alegre, Passo Fundo, Bento Gonçalves, Gravataí, Santa Maria e Rio Grande. </span>
         </div>
     </div>
 );
