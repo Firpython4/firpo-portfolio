0909c9c32bec44b254b2a598960895b93e07c3bc
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:50:00 -0300

    Moved classNames

--- src/components/bottomBar.tsx
+++ src/components/bottomBar.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image"
 import Link from "next/link";
 import { emailLink, linkedInLink } from "~/config";
+import {locale} from "~/localization/localization";
 
 export const BottomBar = (props: {className?: string}) =>
 (
-    <div className={`${props.className} flex flex-col gap-y-[11px] items-center justify-center md:flex-row md:gap-x-[11px]`}>
+    <div
+        className={`${props.className} flex flex-col gap-y-[11px] items-center justify-center md:flex-row md:gap-x-[11px]`}>
         <p className={`w-[412px] text-black flex items-center text-xl font-semibold font-inter leading-normal`}>
-            Resolva seus problemas de comunicação:
+            {locale.solveYourCommunicationProblems}
         </p>
         <div className="m-0 flex flex-row gap-x-[26px]">
             <Link href={emailLink}>
