7f2bbfabf274e8be8824e54e3957c4c26b3bc784
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:47:50 -0300

    Made icons clickable

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
+import { emailLink, linkedInLink } from "../config";
 
 export const NavBar = () =>
 {
     return <div className="flex flex-row space-x-[26px]">
-        <Image alt="email" src="/icons/hero-icons/email-icon.svg" width={38.48} height={34.2}/>
-        <Image alt="LinkedIn" src="/icons/hero-icons/linkedin-icon.svg" width={34.21} height={34.21}/>
+        <Link href={emailLink}>
+            <Image alt="email" src="/icons/hero-icons/email-icon.svg" width={38.48} height={34.2}/>
+        </Link>
+        <Link href={linkedInLink}>
+            <Image alt="LinkedIn" src="/icons/hero-icons/linkedin-icon.svg" width={34.21} height={34.21}/>
+        </Link>
     </div>;
 };
