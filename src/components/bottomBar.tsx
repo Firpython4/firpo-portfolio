ce7a7d453f83935fac36a67b0669affdeaf09e5a
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:50:18 -0300

    Added success message after submission

--- src/components/bottomBar.tsx
+++ src/components/bottomBar.tsx
@@ -9,7 +9,7 @@ export const BottomBar = (props: PropsWithClassName<PropsWithLocalizedCopy>) =>
     <div
         className={`${props.className} flex flex-col gap-y-[11px] items-center justify-center md:flex-row md:gap-x-[11px]`}>
         <p className={`xl:w-[412px] text-black text-center flex items-center text-lg ${responsiveText} font-semibold font-inter leading-normal`}>
-            {props.localizedCopy.solveYourCommunicationProblems}
+            {props.localizedCopy.home.callToAction.solveYourCommunicationProblems}
         </p>
         <NavBar iconPaths={{email: "/icons/email-icon.svg", linkedin: "/icons/linkedin-icon.svg"}}/>
     </div>
