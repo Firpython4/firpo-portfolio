5b59498d74087904433942aaddffce93b53cc1fc
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:49:57 -0300

    Fixed vertical spacing

--- src/components/expositionText.tsx
+++ src/components/expositionText.tsx
@@ -3,9 +3,9 @@ import {locale} from "~/localization/localization";
 export const ExpositionText = () =>
 (
     <div
-        className="w-full flex flex-col md:flex-row justify-center pt-[118px] gap-[40px] md:gap-[80px] lg:gap-[130] xl:gap-[215px]">
+        className="w-full flex flex-col lg:flex-row justify-center max-lg:items-center pt-[118px] gap-[40px] md:gap-[20px] lg:gap-[130px] xl:gap-[215px]">
         <div
-            className={`w-[434px] h-[535px] text-black text-small not-italic font-semibold leading-[27px] font-inter`}>{locale.expositionFirst}
+            className={`w-[434px] text-black text-small not-italic font-semibold leading-[27px] font-inter`}>{locale.expositionFirst}
         </div>
         <div
             className="w-[435px] text-black text-small not-italic font-normal leading-[27px] tracking-[-0.17px] pt-[172px] font-inter"><span
