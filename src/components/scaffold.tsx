2cfd0ad7cb42139d72038d7d764a5ab81761d6da
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:47:45 -0300

    Wrapped up landing page

--- src/components/scaffold.tsx
+++ src/components/scaffold.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 import { heptaSlab } from "../fonts/heptaSlab";
 import { inter } from "../fonts/inter";
 
-export const Main = (props: PropsWithChildren) => (
+export const Scaffold = (props: PropsWithChildren) => (
     <main className={`bg-white flex flex-col justify-center overflow-hidden ${inter.variable} ${heptaSlab.variable}`}>
         {props.children}
     </main>
