946b5fd912c7b6e718db7c55e591e29af5e2afe9
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:49:53 -0300

    Implemented work template

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 import background from "../../public/firpo-color.png";
-import { HorizontalCenter } from "./horizontalCenter";
+import { VerticalCenterBox } from "./verticalCenterBox";
 import { NavBar } from "./navBar";
 
 export const Hero = () =>
 (
-    <div className=" w-full overflow-hidden">
-        <HorizontalCenter>
-            <div className="relative w-[1920px] h-[933px]">
-                <div className="absolute bg-gradient-to-r from-black/5 to-black/10"/>
+    <div className=" w-responsive-screen overflow-hidden">
+        <VerticalCenterBox>
+            <div className="relative w-responsive-screen h-responsive-screen">
                 <div className="relative top-[-120px]">
-                    <Image width={1920} height={933} src={background.src} alt="Marcelo Firpo"/>
+                    <Image width={5760} height={2799} src={background.src} alt="Marcelo Firpo"/>
                 </div>
                 <h2 className={`absolute w-[752px] shrink-0 text-white text-[107px] not-italic font-semibold leading-[98px] tracking-[-3.745px] left-[54.63%] top-[27.22%]  font-hepta_slab`}>
                     Marcelo Firpo
@@ -18,8 +17,10 @@ export const Hero = () =>
                 <h3 className={`absolute w-[500px] h-[345px] text-white text-[44px] not-italic font-bold leading-[50px] left-[54.73%] top-[54.76%] font-hepta_slab`}>
                     CMO, Diretor de Criação, Consultor Criativo, Redator, Roteirista
                 </h3>
-                <NavBar/>
+                <div className="absolute right-[21.56%] top-[13.61%]">
+                    <NavBar/>
+                </div>
             </div>
-        </HorizontalCenter>
+        </VerticalCenterBox>
     </div>
 );
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const NavBar = () =>
 {
-    return <div className="absolute left-[73.28%] top-[13.61%] flex flex-row space-x-[26px]">
+    return <div className="flex flex-row space-x-[26px]">
         <Image alt="email" src="/icons/hero-icons/email-icon.svg" width={38.48} height={34.2}/>
         <Image alt="LinkedIn" src="/icons/hero-icons/linkedin-icon.svg" width={34.21} height={34.21}/>
     </div>;
