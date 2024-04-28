a0430a84758c2b07fa45da0d51d647b7ac8377d4
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:48:42 -0300

    Renamed tcms to typefs

--- src/cms/cmsSchemas.ts
+++ src/cms/cmsSchemas.ts
@@ -1,24 +1,28 @@
 import { z } from "zod";
-import { tcms } from "./type-fs/schemas";
+import { typefs } from "./type-fs/schemas";
 
-const videoWithThumbnail = tcms.object({
-  url: tcms.url(),
-  thumbnail: tcms.image("public"),
+const videoWithThumbnail = typefs.object({
+  url: typefs.url(),
+  thumbnail: typefs.image("public"),
 });
 
-const videoUrl = tcms.url();
+const videoUrl = typefs.url();
 
-export const piece = tcms.union(videoUrl, videoWithThumbnail, tcms.image("public"));
+export const piece = typefs.union(
+  videoUrl,
+  videoWithThumbnail,
+  typefs.image("public"),
+);
 
-const pieces = tcms.array(piece).withName("pieces");
+const pieces = typefs.array(piece).withName("pieces");
 
-export const collection = tcms
+export const collection = typefs
   .object({
     pieces,
-    thumbnail: tcms.object({ thumbnail: piece }).withName("thumbnail"),
-    pt: tcms.markdown("pt").withMatter(z.object({ title: z.string() })),
-    en: tcms.markdown("en").withMatter(z.object({ title: z.string() })),
+    thumbnail: typefs.object({ thumbnail: piece }).withName("thumbnail"),
+    pt: typefs.markdown("pt").withMatter(z.object({ title: z.string() })),
+    en: typefs.markdown("en").withMatter(z.object({ title: z.string() })),
   })
   .withName();
 
-export const collections = tcms.array(collection);
+export const collections = typefs.array(collection);
