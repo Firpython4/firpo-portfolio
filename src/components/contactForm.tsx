1b6b92e6e2b9e785f92b68aef8944250ba9f3b3f
Santiago Firpo <santiagofirpo4@gmail.com>
2024-04-28 00:48:15 -0300

    Added success message after submission

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,9 +1,10 @@
 "use client"
 
+import { options } from "prettier-plugin-tailwindcss";
 import {
     type FieldErrors, type FieldPath,
     type FieldValues, type RegisterOptions,
-    useForm,
+    useForm, UseFormProps,
     type UseFormRegister,
 } from "react-hook-form";
 import { z } from "zod";
@@ -36,7 +37,7 @@ function FormItem<FieldValuesType extends FieldValues, FieldNameType extends Fie
     {
         return (
             <div className={`flex flex-col ${props.className}`}>
-                {<p className="text-red-500">{errorMessages}</p>}
+                {<p className="text-red-400">{errorMessages}</p>}
                 <input className={`${formClassNames}`}
                        type="text"
                        placeholder={props.placeholder} {...props.register(props.name, props.options)}/>
@@ -62,7 +63,7 @@ function FormArea<FieldValuesType extends FieldValues, FieldNameType extends Fie
     {
         return (
             <div className={`flex flex-col ${props.gridClassNames}`}>
-                {<p className="text-red-500">{errorMessages}</p>}
+                {<p className="text-red-400">{errorMessages}</p>}
                 <textarea className={`${formBorderClassNames} h-full ${props.className}`}
                        placeholder={props.placeholder} {...props.register(props.name, props.options)}/>
             </div>
@@ -82,11 +83,19 @@ function FormArea<FieldValuesType extends FieldValues, FieldNameType extends Fie
 
 const ContactForm = (props: PropsWithClassName<{locale: Locale, copy: LocalizedCopy}>) =>
 {
-    const formProps = {
-        resolver: zodResolver(ContactFormValues(props.copy.home.contactForm.errors))
+    const formProps: UseFormProps<ContactFormType> = {
+        resolver: zodResolver(ContactFormValues(props.copy.home.contactForm.errors)),
+        defaultValues: {
+            Email: "",
+            Content: "",
+            Subject: "",
+            "Last Name": "",
+            "First Name": ""
+        }
     };
 
-    const { register, handleSubmit, formState: { errors } } = useForm<ContactFormType>(formProps);
+    const { register, handleSubmit, getValues, formState: { defaultValues, errors, isSubmitSuccessful, isDirty,  }, reset } = useForm<ContactFormType>(formProps);
+    console.log(isDirty);
     const contactForm = props.copy.home.contactForm;
     const submitContactForm = async (data: ContactFormType) =>
     {
@@ -98,21 +107,26 @@ const ContactForm = (props: PropsWithClassName<{locale: Locale, copy: LocalizedC
         {
             //Okay to catch the error here. Google Forms won't return a well-formed response.
         }
+        
+        reset();
     };
 
-    return <form className={`${props.className} font-inter flex flex-col gap-y-8 items-center relative`} onSubmit={handleSubmit(submitContactForm)}>
-        <div className="grid grid-cols-2 grid-rows-6 gap-x-3 gap-y-4">
-            <FormItem placeholder={contactForm.firstName} name="First Name" register={register} options={{required: true}} errors={errors}/>
-            <FormItem placeholder={contactForm.lastName} name="Last Name" register={register} options={{required: true}} errors={errors}/>
-            <FormItem className="col-span-2" placeholder={contactForm.email} name="Email" register={register} options={{required: true}} errors={errors}/>
-            <FormItem className="col-span-2" placeholder={contactForm.subject} name={"Subject"} register={register} errors={errors}/>
-            <FormArea gridClassNames="col-span-2 row-span-4" className="px-2 py-1 resize-none" placeholder={contactForm.content} name="Content" register={register} options={{required: true}} errors={errors}/>
-        </div>
-        <input className="border-2 border-gray-300 rounded-md w-[20%] cursor-pointer"
-               type="submit"
-               value="Send"
-               lang={props.locale}/>
-    </form>
+    return <div className={`${props.className} font-inter flex flex-col items-center gap-y-6`}>
+        {(isSubmitSuccessful) && <p className="text-green-500">{contactForm.submitSuccessful}</p>}
+        <form className="flex flex-col gap-y-8 items-center relative" onSubmit={handleSubmit(submitContactForm)}>
+            <div className="grid grid-cols-2 grid-rows-6 gap-x-3 gap-y-4">
+                <FormItem placeholder={contactForm.firstName} name="First Name" register={register} options={{required: true}} errors={errors}/>
+                <FormItem placeholder={contactForm.lastName} name="Last Name" register={register} options={{required: true}} errors={errors}/>
+                <FormItem className="col-span-2" placeholder={contactForm.email} name="Email" register={register} options={{required: true}} errors={errors}/>
+                <FormItem className="col-span-2" placeholder={contactForm.subject} name={"Subject"} register={register} errors={errors}/>
+                <FormArea gridClassNames="col-span-2 row-span-4" className="px-2 py-1 resize-none" placeholder={contactForm.content} name="Content" register={register} options={{required: true}} errors={errors}/>
+            </div>
+            <input className="border-2 border-gray-300 rounded-md w-[20%] cursor-pointer"
+                   type="submit"
+                   value="Send"
+                   lang={props.locale}/>
+        </form>
+    </div>
 }
 
 export default ContactForm;
