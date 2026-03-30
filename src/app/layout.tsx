d54ba635a57a3df288db9fd3968f8166b48180d3
Santiago Firpo <santiagofirpo4@gmail.com>
2026-03-30 17:51:41 -0300

    Changed about font to editorial typeface

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import { type PropsWithChildren } from "react";
 import { heptaSlab } from "~/fonts/heptaSlab";
 import { inter } from "~/fonts/inter";
+import { playfairDisplay } from "~/fonts/playfairDisplay";
+import { dmSans } from "~/fonts/dmSans";
 import "~/styles/globals.css";
 
 const RootLayout = (props: PropsWithChildren) => {
   return (
-    <html lang="pt" className={`${inter.variable} ${heptaSlab.variable}`}>
+    <html
+      lang="pt"
+      className={`${inter.variable} ${heptaSlab.variable} ${playfairDisplay.variable} ${dmSans.variable}`}
+    >
       <body>{props.children}</body>
     </html>
   );
