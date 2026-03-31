cbd8d4dfee7ac3e53152166bd6b8acbe8aff94d1
Santiago Firpo <santiagofirpo4@gmail.com>
2026-03-30 22:39:35 -0300

    Increased text size

--- src/app/[locale]/(home)/layout.tsx
+++ src/app/[locale]/(home)/layout.tsx
@@ -18,7 +18,7 @@ const RootLayout = (
       <body className="relative bg-background text-charcoal antialiased">
         {props.children}
         <LocaleSwitcher
-          className="absolute right-[clamp(70px,18vw,180px)] top-4 font-body text-sm tracking-wide text-white max-[200px]:hidden md:top-6 lg:top-12"
+          className="absolute right-[clamp(70px,18vw,180px)] top-4 pt-1 font-body text-base tracking-wide text-white max-[200px]:hidden md:top-6 lg:top-12"
           locale={props.params.locale}
         />
       </body>
